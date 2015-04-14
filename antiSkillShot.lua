8c8f3f83c3b54503d2c6f6b0561e3305b07cb37e
Tim O'Connor <issuefree@gmail.com>
2015-04-14 15:14:19 -0600

    BoL conversion

--- antiSkillShot.lua
+++ antiSkillShot.lua
@@ -11,10 +11,10 @@ function assTick()
          local spells = GetSpellShots(enemy.name)
          if spells then
             for _,spell in pairs(spells) do
-               if spell.perm and spell.key and enemy["SpellLevel"..spell.key] > .5 then
+               if spell.perm and spell.key and GetSpellInfo(spell.key).level > .5 then
 
                   if spell.ss and spell.isline then
-                     if GetDistance(enemy) < spell.range and enemy["SpellTime"..spell.key] >= 1 then
+                     if GetDistance(enemy) < GetSpellRange(spell) and GetSpellInfo(spell.key).currentCd < 1 then
                         if spell.block then
                            local unblocked = GetUnblocked(spell, enemy, MYMINIONS, ALLIES)
                            for _,test in ipairs(unblocked) do
@@ -29,8 +29,8 @@ function assTick()
                      end
                   end
 
-                  if not spell.ss and enemy["SpellTime"..spell.key] >= 1 then
-                     Circle(enemy, spell.range, red, 2)
+                  if not spell.ss and GetSpellInfo(spell.key).currentCd < 1 then
+                     Circle(enemy, GetSpellRange(spell), red, 2)
                   end
 
                end
