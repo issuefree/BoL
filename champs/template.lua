f283ce6a004cd816c382c98d847a2ca6bd480506
Tim O'Connor <issuefree@gmail.com>
2015-05-01 08:37:09 -0600

    Minor update to template.

--- champs/template.lua
+++ champs/template.lua
@@ -10,7 +10,7 @@ require "issuefree/modules"
 pp("\nTim's Template")
 
 InitAAData({ 
---    speed = 1300, windup=.25,
+--    speed = 1300,
 --    minMoveTime = 0,
 --    extraRange=-20,
 --    particles = {"TeemoBasicAttack_mis", "Toxicshot_mis"} 
