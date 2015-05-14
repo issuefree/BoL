0d30ed74e4c0fe21653c350a5d1df34612905eb2
Tim O'Connor <issuefree@gmail.com>
2015-05-14 12:25:42 -0600

    Rework ashe for 5.9

--- champs/ashe.lua
+++ champs/ashe.lua
@@ -1,19 +1,21 @@
 require "issuefree/timCommon"
 require "issuefree/modules"
 
+-- TODO I may want to incorporate the new frost shot mechanics into last hitting.
+
 pp("\nTim's Ashe")
 
 SetChampStyle("marksman")
 
 InitAAData({
-   speed = 2000,
+   speed = 2500, -- patch notes
    minMoveTime = .25, -- ashe can't get move commands too early for some reason
    particles = {"Ashe_Base_BA_mis", "Ashe_Base_Q_mis"},
-   attacks = {"attack", "frostarrow"}
+   attacks = {"attack"}
 })
 
-AddToggle("frost", {on=true, key=112, label="Auto frost"})
-AddToggle("tear", {on=true, key=113, label="Charge Tear"})
+AddToggle("", {on=true, key=112, label=""})
+AddToggle("", {on=true, key=113, label=""})
 AddToggle("", {on=true, key=114, label=""})
 AddToggle("", {on=true, key=115, label=""})
 
@@ -21,8 +23,14 @@ AddToggle("lasthit", {on=true, key=116, label="Last Hit", auxLabel="{0} / {1}", 
 AddToggle("clear", {on=false, key=117, label="Clear Minions"})
 AddToggle("move", {on=true, key=118, label="Move"})
 
-spells["frost"] = {
-   key="Q"
+spells["focus"] = {
+   key="Q",
+   cost=50
+}
+spells["flurry"] = {
+   key="Q",
+   base=0,
+   ad={.15,.2,.25,.3,.35}
 }
 
 spells["volley"] = {
@@ -34,14 +42,12 @@ spells["volley"] = {
    delay=.25,
    speed=9,
    cone=24.32*2, 
-   cost=60,
-   width=25
+   cost=50,
+   width=20
 }
 
 spells["hawkshot"] = {
    key="E", 
-   range={2500,3250,4000,4750,5500},
-   color=blue
 }
 
 spells["arrow"] = {
@@ -57,21 +63,20 @@ spells["arrow"] = {
    spellName="EnchantedCrystalArrow"
 }
 
+spells["AA"].damOnTarget = function(target)
+   if target and HasBuff("frosted", target) then
+      return me.totalDamage*(.1 + me.critChance)
+   end
+end
+
 function Run()
+   spells["AA"].bonus = 0
+   if P.flurry then
+      spells["AA"].bonus = GetSpellDamage("flurry")
+   end
+
    if StartTickActions() then
       return true
-   end
-
-   if IsOn("frost") then
-      if GetMPerc(me) > .5 and CanChargeTear() or not Alone() then
-         CastBuff("frost")
-      else      
-         CastBuff("frost", false)
-      end
-   end
-
-   if IsOn("tear") then
-      UseItem("Muramana")
    end
 
    -- TODO should write an auto hawkshot for people that run into brush
@@ -95,17 +100,19 @@ function Run()
       end
    end
 
-   if IsOn("tear") and CanChargeTear() and P.frost then
-      if HitMinion("AA", "strong") then
-         return true
-      end      
-   end
-
    EndTickActions()
 end
 
 function Action()   
    -- TestSkillShot("arrow")
+
+   if CanUse("focus") and Q.focus then
+      if #GetInE2ERange(me, GetAARange()*.75, ENEMIES) >= 1 then
+         Cast("focus", me)
+         PrintAction("Focus for flurry")
+         return true
+      end
+   end
 
    if CastBest("volley") then
       return true
@@ -125,7 +132,10 @@ end
    
 
 local function onObject(object)
-   PersistBuff("frost", object, "Ashe_Base_q_buf", 125)
+   PersistBuff("focus", object, "Ashe_Base_Q_ready")
+   PersistBuff("flurry", object, "Ashe_Base_Q_buf.troy")
+
+   PersistOnTargets("frosted", object, "Ashe_Base_freeze", ENEMIES, MINIONS, CREEPS)
 end
 
 local function onSpell(unit, spell)
